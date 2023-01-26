7a98092fa27d46f7dc3a4f36f571dd8a68ad5a34
Gauthier Guyaz <107797232+Gauthier23@users.noreply.github.com>
2023-01-26 16:29:39 +0100

    Update Forest_Fire.py

--- Forest_Fire.py
+++ Forest_Fire.py
@@ -5,58 +5,75 @@ Created on Sun Jan  1 16:17:57 2023
 @author: Gauthier Guyaz
 """
 
-
-from tkinter import*
-from math import*
-from random import*
+import tkinter as tk        #gestion graphismes
+from random import randint  #germe aléatoire
+import WindVector           #importation d'un fichier annexe 
+from PIL import Image       #gestion du raster
 
 
 # =============================================================================
-#               I PARTIE, GESTION GENERALE DE L'AUTOMATE
+#             I PARTIE, VARIABLES ET CONSTANTES GLOBALES
+# =============================================================================
+
+SIDE = 600                     #taille du côté de la grille (entre 500-650)
+CELL = 4                       #taille du côté d'une cellule
+BACKGROUND_COLOR = "#FFFFFF"   #couleur de fond du canvas principal
+dico = {}                      #dictionaire principal: clé = position (x,y), valeur = état (1,2,3)
+temp = {}                      #idem mais temporaire (relai du dico pour génération n+1)
+gen_count = 0
+
+#-les états (alphabet) de l'automate-#
+# Reste du biome = 0  ==> None
+# arbre = 1           ==> vert
+# feu = 2             ==> rouge
+# cendre = 3          ==> gris
+
+
+# =============================================================================
+#               II PARTIE, GESTION GENERALE DE L'AUTOMATE
 # =============================================================================
 
 ###---Fonction dessinant la matrice---###
     
 def dam():
     #-double boucle parcourant chaque cellule du canvas-#
-    for y in range(0, side, cell):
-        for x in range(0, side, cell):
+    for y in range(0, SIDE, CELL):
+        for x in range(0, SIDE, CELL):
             #-création d'une cellule associée à une position-#
-            can.create_rectangle(x, y, x + cell, y + cell, outline="black", fill="")
+            can.create_rectangle(x, y, x + CELL, y + CELL, outline="black", fill="")
             #-initialisation de la cellule à 0 dans le dictionnaire-#
-            dico[x/cell, y/cell] = 0 
-   
+            dico[int(x/CELL), int(y/CELL)] = 0 
     
 ###---Fonction modifiant l'état de la cellule cliquée---###
 
 def click_gauche(event):
     global dico
-    x = event.x -(event.x%cell)     #prise de coordonnée x par la soustraction du modulo
-    y = event.y -(event.y%cell)     #prise de coordonnée y par la soustraction du modulo
+    x = event.x -(event.x%CELL)     #prise de coordonnée x par la soustraction du modulo
+    y = event.y -(event.y%CELL)     #prise de coordonnée y par la soustraction du modulo
     #-si la cellule a déjà un état, annihilation et dico à 0-#
-    if dico[x/cell,y/cell] == 1 or dico[x/cell,y/cell] == 2:
-        can.create_rectangle(x, y, x + cell, y + cell, outline = "black", fill=backgroud_color)
-        dico[x/cell,y/cell] = 0
+    if dico[x/CELL,y/CELL] == 1 or dico[x/CELL,y/CELL] == 2:
+        can.create_rectangle(x, y, x + CELL, y + CELL, outline = "black", fill=BACKGROUND_COLOR)
+        dico[int(x/CELL),int(y/CELL)] = 0
     #-si la cellule n'a pas d'état, création d'une case verte et dico à 1-#
-    elif dico[x/cell,y/cell] == 0:
-        can.create_rectangle(x, y, x+cell, y+cell, fill='darkgreen')
-        dico[x/cell,y/cell] = 1
+    elif dico[x/CELL,y/CELL] == 0:
+        can.create_rectangle(x, y, x+CELL, y+CELL, fill='darkgreen')
+        dico[int(x/CELL),int(y/CELL)] = 1
   
     
 ###---Fonction mettant le feu à la cellule cliquée---###    
 
 def click_droit(event): 
-    x = event.x -(event.x%cell)     #prise de coordonnée x par la soustraction du modulo
-    y = event.y -(event.y%cell)     #prise de coordonnée x par la soustraction du modulo
+    x = event.x -(event.x%CELL)     #prise de coordonnée x par la soustraction du modulo
+    y = event.y -(event.y%CELL)     #prise de coordonnée x par la soustraction du modulo
     #-création d'une case rouge et dico à 2-#
-    can.create_rectangle(x, y, x+cell, y+cell, fill ='red')
-    dico[x/cell,y/cell]=2           
+    can.create_rectangle(x, y, x+CELL, y+CELL, fill ='red')
+    dico[int(x/CELL),int(y/CELL)]=2           
 
 
 ###---Fonction remplissant les carrés selon la liste temporaire---###
 
 def draw():
-    can.delete(ALL)  # supression de l'ancien Canvas
+    can.delete(tk.ALL)  # supression de l'ancien Canvas
     dam()            # recréation via fonction de la matrice neutre
     
     #-parcours des coordonnées dans le dictionnaire temp-#
@@ -72,14 +89,12 @@ def draw():
         elif temp[x, y] == 3: 
             fill = "grey"
             
-        can.create_rectangle(cell * x, cell * y, cell * x + cell, cell * y + cell, fill=fill) 
+        can.create_rectangle(CELL * x, CELL * y, CELL * x + CELL, CELL * y + CELL, fill=fill) 
     #-remplacement du dictionnaire par le temporaire (relais)-#   
     dico.update(temp)  
     
       
 ###---Fonction calculant les cellules touchées par le feu---###    
-
-import WindVector  #importation d'un fichier annexe 
 
 def calculer():
     global temp, gen_count
@@ -104,23 +119,20 @@ def calculer():
                 if dico.get((x + coord[0], y + coord[1]), 0) == 1:
                     temp[x + coord[0], y + coord[1]] = 2
                     
-                    
             #-analyse du second voisinage (vent)-#
             for coord in wind_coord:
                 #-si une cellule verte est touchée par cette liste, elle brûle-#
                 if dico.get((x + coord[0], y + coord[1]), 0) == 1:
                     temp[x + coord[0], y + coord[1]] = 2
      
-    #-gestion du comptage des générations-#    
+    #-gestion du comptage des générations-# 
     gen_count += 1      
     gen.set(gen_count)
     
     draw()  #dessin de la nouvelle génération
     
-    
-
 # =============================================================================
-#               II PARTIE, MODULES D'INTERACTION UTILISATEUR
+#               III PARTIE, MODULES D'INTERACTION UTILISATEUR
 # =============================================================================
 
 ###---Fonctions gérant la répétition des générations selon le temps---###
@@ -133,7 +145,7 @@ def withtime():
 def update_calculs():
     calculer()
     if flag == 1:
-            fen.after(100, update_calculs)
+            fen.after(200, update_calculs)
         
 def stop():
     global flag
@@ -143,7 +155,7 @@ def stop():
 ###---Fonction permettant de recommencer un nouveau germe---###
 
 def reset():
-    global temp, dico, gen_count
+    global temp, gen_count
     gen_count = 0
     gen.set(gen_count)
     temp = {}
@@ -154,15 +166,19 @@ def reset():
     
 def destroy():
     fen.destroy()     
+
     
 ###---Fonction permettant de créer un germe aléatoire---###
+
 def alea():
     propo = curseur_propo.get()
     dicoPropo = {}
-    for x in range(0, side//cell):
-        for y in range(0, side//cell):
-            r = randint(1,100)
-            if r <= propo:
+    #-boucles parcourant chaque cellule-#
+    for x in range(0, SIDE//CELL):
+        for y in range(0, SIDE//CELL):
+            #-si les probabilité appartiennent à notre pourcentage-#
+            if randint(1,100) <= propo:
+                #-ajout d'un arbre-#
                 dicoPropo[x,y] = 1
     temp.update(dicoPropo)
     draw()    
@@ -170,127 +186,116 @@ def alea():
     
 ###---Ajout de la MAP raster---###
 
-def MapLidar():
+def mapLidar():
     global temp
-    temp.update(dico)
     temp.update(dicoLAS)
     draw()    
     
     
 ###---Importation du raster---###    
 
-from PIL.Image import*
+img = Image.open("OGGIOMVP.png")
 
-i = open("image.png")
+(width,height) = img.size
 
-(width,height) = i.size     
-dicoLAS = {}
+#-dictionnaire vide pour stocker les infos de l'image-#
+dicoLAS = {} 
 
+#-boucles pour parcourir tous les pixels de l'image-#
 for x in range(0, width):
     for y in range(0, height):
-        c = Image.getpixel(i, (x, y)) 
-        if c == 42:     
+        
+        #-récupération de la valeur du pixel à la position (x, y)-#
+        pixel = img.getpixel((x, y)) 
+        
+        #-si la valeur du pixel est égale à 31 (arbre)-#
+        if pixel == 31:       
+            #-ajout de la position (x, y) dans le dicoLAS avec la valeur 1-#
             dicoLAS[x,y] = 1
-
-
-# =============================================================================
-#             III PARTIE, VARIABLES ET CONSTANTES GLOBALES
-# =============================================================================
-
-side = 500                         #taille du côté du canvas carré dans lequel l'automate se développe (entre 500-650)
-cell = 5                           #nombre de pixel pour le côté d'une cellule
-dico = {}                          #dictionaire principal contenant comme clé la position (x,y) et comme valeur son état (1,2,3)
-temp = {}                          #idem mais temporaire afin de gérer la transition vers un nouveau dictionaire principal
-backgroud_color = "#FFFFFF"        #couleur de fond du canvas principal (#D4E6F1)
-gen_count = 0
-# Reste du biome  = 0  ==> None
-# arbre = 1            ==> vert
-# feu = 2              ==> rouge
-# cendre = 3           ==> gris
+        
+# ATTENTION, pour utiliser un fichier lidar dis "rasterisé" disponible, mettre SIDE = 600 et CELL = 4
 
 # =============================================================================
 #            IV PARTIE, GESTIONNAIRE GRAPHISME AVEC TKINTER    
 # =============================================================================
 
-fen = Tk()
+fen = tk.Tk()
 fen.title('SIMULATION WINDOW')
 fen.resizable(False, False)
 
 ###---Création du Frame contenant les boutons latéraux---###
 
-frame2 = Frame(fen)
-frame2.pack(side = RIGHT, fill = "both",padx = 10, pady = 30)
+frame2 = tk.Frame(fen)
+frame2.pack(side = tk.RIGHT, fill = "both",padx = 10, pady = 30)
 
 #-Création des boutons-#
-b1 = Button(frame2, text='next gen', command= calculer )
-b2 = Button(frame2, text='start', command = withtime )
-b3 = Button(frame2, text='stop', command = stop)
-b6 = Button(frame2, text='reset', command = reset)
-b7 = Button(frame2, text='quit', command = destroy)
+b1 = tk.Button(frame2, text='next gen', command= calculer )
+b2 = tk.Button(frame2, text='start', command = withtime )
+b3 = tk.Button(frame2, text='stop', command = stop)
+b6 = tk.Button(frame2, text='reset', command = reset)
+b7 = tk.Button(frame2, text='quit', command = destroy)
 
 #-Empilement boutons-#
-b1.pack(side = TOP, padx=5, pady=5)
-b2.pack(side = TOP, padx=5, pady=5)
-b3.pack(side = TOP, padx=5, pady=5)
-b6.pack(side = TOP, padx=5, pady=5)
-b7.pack(side = TOP, padx=5, pady=5)
+b1.pack(side = tk.TOP, padx=5, pady=5)
+b2.pack(side = tk.TOP, padx=5, pady=5)
+b3.pack(side = tk.TOP, padx=5, pady=5)
+b6.pack(side = tk.TOP, padx=5, pady=5)
+b7.pack(side = tk.TOP, padx=5, pady=5)
 
 ###---Création du "sous-frame" contenant le gestionnaire du vent---###
 
-frame2_1 = Frame(frame2, borderwidth=2, relief=GROOVE)
-frame2_1.pack(side = TOP, fill = "both", pady = 7)
+frame2_1 = tk.Frame(frame2, borderwidth=2, relief=tk.GROOVE)
+frame2_1.pack(side = tk.TOP, fill = "both", pady = 7)
 
 #-Labels informatifs pour les curseurs-#
-wind = Label(frame2_1, text = "WIND")
-speed_angle = Label(frame2_1, text = "speed" + " "*10 + "angle")   
+wind = tk.Label(frame2_1, text = "WIND")
+speed_angle = tk.Label(frame2_1, text = "speed" + " "*10 + "angle")   
 
-wind.pack(side = TOP, padx = 5)
-speed_angle.pack(side = TOP)
+wind.pack(side = tk.TOP, padx = 5)
+speed_angle.pack(side = tk.TOP)
 
 #-Curseurs-#
-curseur_speed = Scale(frame2_1, from_=0, to=30, orient=VERTICAL, length=330, width=15, sliderlength=20)
-curseur_angle = Scale(frame2_1, from_=0, to=360, orient=VERTICAL, length=330, width=15, sliderlength=20)
-curseur_speed.pack(side = LEFT)
-curseur_angle.pack(side = RIGHT)
+curseur_speed = tk.Scale(frame2_1, from_=0, to=30, orient=tk.VERTICAL, length=330, width=15, sliderlength=20)
+curseur_angle = tk.Scale(frame2_1, from_=0, to=360, orient=tk.VERTICAL, length=330, width=15, sliderlength=20)
+curseur_speed.pack(side = tk.LEFT)
+curseur_angle.pack(side = tk.RIGHT)
 
 ###---Création du Frame contenant le canvas et les informations sur la simulation---###
 
-frame = Frame(fen)
+frame = tk.Frame(fen)
 frame.pack(fill="both", padx = 5)
 
 #-Titre simulation-#
-Title = Label(frame, text ='Forest Fire',font = ("Helvetica", 12))
+Title = tk.Label(frame, text ='Forest Fire',font = ("Helvetica", 12))
 Title.pack(padx=7, pady=5)
 
 #-Création du canvas principal (simulation)-#
-can = Canvas(frame, width = side, heigh = side, bg = backgroud_color) 
-can.pack(side=TOP, padx=10, pady=0)
+can = tk.Canvas(frame, width = SIDE, heigh = SIDE, bg = BACKGROUND_COLOR) 
+can.pack(side=tk.TOP, padx=10, pady=0)
 can.bind("<Button-1>", click_gauche)
 can.bind("<Button-3>", click_droit)
 
 #-Création des boutons permettant de générer des germes-#
-b4 = Button(frame, text='input Lidar file', command = MapLidar) 
-b5 = Button(frame, text='input propo', command = alea) 
-b4.pack(side = LEFT, padx=5, pady=5)
-b5.pack(side = LEFT, padx=5, pady=5)
+b4 = tk.Button(frame, text='input Lidar file', command = mapLidar) 
+b5 = tk.Button(frame, text='input propo', command = alea) 
+b4.pack(side = tk.LEFT, padx=5, pady=5)
+b5.pack(side = tk.LEFT, padx=5, pady=5)
 
 #-Curseur pour gérer la proportion du germe aléatoire-#
-curseur_propo = Scale(frame, from_=0, to=100, orient=HORIZONTAL, length=200, width=20, sliderlength=20, tickinterval=25)
+curseur_propo = tk.Scale(frame, from_=0, to=100, orient=tk.HORIZONTAL, length=200, width=20, sliderlength=20, tickinterval=25)
 curseur_propo.set(50)
-curseur_propo.pack(side = LEFT)
+curseur_propo.pack(side = tk.LEFT)
 
 #-Affichage du nombre de génération-#
-generationtxt = Label(frame, text= "Generation :")
-generationtxt.pack(side = LEFT, padx = 20)
+generationtxt = tk.Label(frame, text= "Generation :")
+generationtxt.pack(side = tk.LEFT, padx = 20)
 
-gen = IntVar()
-generation = Label(frame, textvariable = gen )
-generation.pack(side = LEFT)
+gen = tk.IntVar()
+generation = tk.Label(frame, textvariable = gen )
+generation.pack(side = tk.LEFT)
 
 
-#####-----Préparation interface de base-----#####
+#####-----Préparation interface de base et boucle générale-----#####
 
 dam()
 fen.mainloop()
-
-
